1ad3a5210d7ade20f14518484ad165aca1fe8f58
JP Lew <jp@kriyate.co>
2017-10-22 17:35:00 -0700

    going nowhere fast with search results

--- src/app/services/plant-search.service.ts
+++ src/app/services/plant-search.service.ts
@@ -13,7 +13,78 @@ export class PlantSearchService {
 
   search(term: string): Observable<PlantData[]> {
     return this.http.get(`api/plants/?location=${term}`)
-      .map( res => res.json() )
+      .map( res => {
+        // console.log(res.json())
+        const data = res.json()
+        const results = []
+
+        for (let i = 0; i < data.length; i++) {
+
+          const element = data[i]
+
+          let existingRegion = results.find( x => {
+            return x.name === element.region
+          })
+          console.log(existingRegion)
+
+          // if the region has not been added yet, then add it.
+          if (i === 0) {
+            const regionObj: any = {}
+            regionObj.name = element.region
+            results.push(regionObj)
+            existingRegion = regionObj
+            existingRegion.locations = []
+          } else {
+            if (existingRegion) {
+              console.log('Region exists already')
+              return
+            } else {
+              console.log('Region does not exist yet')
+              const regionObj: any = {}
+              regionObj.name = element.region
+              results.push(regionObj)
+              existingRegion = regionObj
+              existingRegion.locations = []
+            }
+          }
+
+          const locationObj = {}
+          locationObj['name'] = element.location
+          locationObj['fpy'] = element.yieldData[4].value
+          existingRegion.locations.push(locationObj)
+
+        }
+
+        console.log(results)
+
+        return results
+
+        // data.forEach(element => {
+
+        //   results.forEach( ( e, i ) => {
+
+        //     console.log(i)
+
+
+        //     if ( e.name && e.name === element.region ) {
+        //       console.log('already exists')
+        //       return
+        //     } else {
+        //       console.log('region doesn\'t exist yet')
+        //       const myRegionObj: any = {}
+        //       myRegionObj.name = element.region
+        //       results.push(regionObj)
+        //     }
+        //   })
+        //   console.log(results)
+
+        //   const currentObj = results.find( obj => {
+        //     return obj.name === element.region
+        //   })
+        //   console.log(currentObj)
+
+        // })
+      })
   }
 
   private handleError(error: any): Promise<any> {
